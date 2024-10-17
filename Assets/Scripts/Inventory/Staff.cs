82f04222d439b019f8cd2a8d88efd6071390b09d
inasekin <nasekinid8591@yandex.ru>
2024-10-17 19:48:24 +0300

    magic laser

--- Assets/Scripts/Inventory/Staff.cs
+++ Assets/Scripts/Inventory/Staff.cs
@@ -26,6 +26,7 @@ public class Staff : MonoBehaviour, IWeapon
     
     public void SpawnStaffProjectileAnimEvent() {
         GameObject newLaser = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+        newLaser.GetComponent<MagicLaser>().UpdateLaserRange(weaponInfo.weaponRange);
     }
 
     public WeaponInfo GetWeaponInfo()
